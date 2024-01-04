09dfd24e36496881cc01058a2b7ae08b41afe143
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-01-04 23:54:46 +0530

    Created frontend components in mid*way

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="todo"/><br /><br />
+        <input type="text" placeholder="description"/><br /><br />
+        <button>Add ToDo</button>
+    </div>
+}
