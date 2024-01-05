fe01997eebb7d9bdde1db53e4264c8fdc979466f
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-01-05 10:36:43 +0530

    Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Todos } from './components/Todos'
-import { CreateTodo } from './components/CreateTodo'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Todos } from "./components/Todos";
+import { CreateTodo } from "./components/CreateTodo";
+import "./App.css";
 
 function App() {
   //state variable
-  const [Todos, setTodos] = useState([]);
-
-  fetch("http://localhost:3000/todos").then(async function(res){
-    const json=await res.json();
-    setTodos(json.todos);
-  })
+  const [todos, setTodos] = useState([]);
 
   return (
     <div>
-      HELLO THERE
       <CreateTodo></CreateTodo>
-      <Todos todos={[
-        {
-          title:"as",
-          description:"sdfsd",
-          completed:false
-        }
-      ]}></Todos>
+      <Todos todos={todos}></Todos>
     </div>
   );
-  
 }
 
-export default App
+export default App;
